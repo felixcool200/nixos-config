6a3ca54c617b31f7e5682f29634c9ccf24694d9a
Felix Söderman <felixsoderman+github@gmail.com>
2026-02-22 19:20:43 +0100

    Update hyprland config to fix config errors

--- hyprland/home.nix
+++ hyprland/home.nix
@@ -52,7 +52,6 @@
       "col.inactive_border" = "rgba(44475aaa)";
       "col.nogroup_border" = "rgba(282a36dd)";
       "col.nogroup_border_active" = "rgb(bd93f9) rgb(44475a) 90deg";
-      no_border_on_floating = false;
       border_size = 2;
       gaps_in = 3; # Default 5
       gaps_out = 6; # Default 10
@@ -84,7 +83,7 @@
     };
 
     # Window rules
-    windowrulev2 = [
+    windowrule = [
       "bordercolor rgb(ff5555),xwayland:1" # check if window is xwayland
     ];
 
