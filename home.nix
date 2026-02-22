01925c85dce2bf499983c4fcdf1775b93cca16aa
Felix Söderman <felixsoderman+github@gmail.com>
2026-02-22 19:39:01 +0100

    Fix upgrade alias

--- home.nix
+++ home.nix
@@ -55,7 +55,7 @@
       gs = "git status";
       rebuildOS = "nixos-rebuild --flake ~/Documents/nixos-config switch --impure --sudo";
       cleanOS = "sudo nix-env --delete-generations +2 --profile /nix/var/nix/profiles/system && nix-env --delete-generations +2 --profile ~/.local/state/nix/profiles/home-manager && sudo nix-collect-garbage -d && nix-collect-garbage -d && sudo journalctl --vacuum-size=50M";
-      upgradeOS = "nix flake update ~/Documents/nixos-config && nixos-rebuild --flake ~/Documents/nixos-config switch --impure --sudo";
+      upgradeOS = "nix flake update --flake ~/Documents/nixos-config && nixos-rebuild --flake ~/Documents/nixos-config switch --impure --sudo";
     };
   };
 
