718d49a4a7c28db7919be1833a256df17c00d09c
Felix Söderman <felixsoderman+github@gmail.com>
2026-04-07 21:56:07 +0200

    ghostty: Add cursor trail

--- modules/features/ghostty.nix
+++ modules/features/ghostty.nix
@@ -3,8 +3,7 @@
     environment.systemPackages = [ pkgs.ghostty ];
     hjem.users.felixcool200.xdg.config.files."ghostty/config".text = ''
       theme = Dracula
-      # theme = CyberpunkScarletProtocol
-      #custom-shader = ~/Documents/nixos-config/ghostty/ghostty-shaders/in-game-crt.glsl
+      custom-shader = ~/Documents/nixos-config/ghostty/shaders/cursor_tail.glsl
     '';
     environment.sessionVariables.TERMINAL = "ghostty";
   };
